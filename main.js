a0717497ae62d82b74810b27eab51ed0ee4d4db2
Clara <clarapachecodev@gmail.com>
2023-02-09 20:30:30 -0300

    Add condition to btnStop event

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const btnPlay = document.querySelector('.play')
 const btnPause = document.querySelector('.pause')
 const btnStop = document.querySelector('.stop')
 const btnSet = document.querySelector('.set')
+const btnSoundOn = document.querySelector('.sound-on')
+const btnSoundOff= document.querySelector('.sound-off')
 
 function togglePlayPauseButtons() {
   btnPlay.classList.toggle('hide')
@@ -14,6 +16,11 @@ function toggleStopSetButtons() {
   btnSet.classList.toggle('hide')
 }
 
+function toggleSoundOnOff() {
+  btnSoundOn.classList.toggle('hide')
+  btnSoundOff.classList.toggle('hide')
+}
+
 btnPlay.addEventListener('click', ()=>{
   togglePlayPauseButtons()
   toggleStopSetButtons()
@@ -21,10 +28,21 @@ btnPlay.addEventListener('click', ()=>{
 
 btnPause.addEventListener('click', ()=>{
   togglePlayPauseButtons()
-  toggleStopSetButtons()
+  
 })
 
 btnStop.addEventListener('click',() => {
-  togglePlayPauseButtons()
+  if(btnPlay.classList.contains('hide')){
+    togglePlayPauseButtons()
+  }
+
   toggleStopSetButtons()
+})
+
+btnSoundOn.addEventListener('click', ()=>{
+  toggleSoundOnOff()
+})
+
+btnSoundOff.addEventListener('click', ()=>{
+  toggleSoundOnOff()
 })
