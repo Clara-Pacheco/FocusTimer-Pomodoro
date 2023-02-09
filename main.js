1bb82e2fcee47ffbe492fc278e67c2e958f4a705
Clara <clarapachecodev@gmail.com>
2023-02-09 19:44:00 -0300

    Add click event for pause and stop buttons

--- main.js
+++ main.js
@@ -3,16 +3,28 @@ const btnPause = document.querySelector('.pause')
 const btnStop = document.querySelector('.stop')
 const btnSet = document.querySelector('.set')
 
-function togglePlayPauseButtons(){
+function togglePlayPauseButtons() {
   btnPlay.classList.toggle('hide')
   btnPause.classList.toggle('hide')
 
 }
 
+function toggleStopSetButtons() { 
+  btnStop.classList.toggle('hide')
+  btnSet.classList.toggle('hide')
+}
+
 btnPlay.addEventListener('click', ()=>{
   togglePlayPauseButtons()
+  toggleStopSetButtons()
 })
 
 btnPause.addEventListener('click', ()=>{
   togglePlayPauseButtons()
+  toggleStopSetButtons()
+})
+
+btnStop.addEventListener('click',() => {
+  togglePlayPauseButtons()
+  toggleStopSetButtons()
 })
