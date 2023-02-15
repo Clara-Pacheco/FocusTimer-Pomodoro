801a0297d85f3f6109a123e9567fee59cf2abd55
Clara <clarapachecodev@gmail.com>
2023-02-15 08:23:47 -0300

    Fix audio loop

--- js/sounds.js
+++ js/sounds.js
@@ -5,8 +5,7 @@ export default function() {
   const bgAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true")
 
 
-  bgAudio.loop
-
+  bgAudio.loop = true
 
  function pressButton() {
   buttonPressAudio.play()
